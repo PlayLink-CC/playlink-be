ca2b4dd1670ce5ed080f0f66d5d543d94da74113
ManojramRagu <manojramragu@gmail.com>
2026-02-06 00:53:13 +0530

    Added wallet top up feature

--- routes/Wallet.js
+++ routes/Wallet.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { authenticate as authMiddleware } from "../middleware/auth.js";
-import { getMyBalance, getMySummary } from "../controllers/WalletController.js";
+import { getMyBalance, getMySummary, topup, confirmTopup } from "../controllers/WalletController.js";
 
 const router = express.Router();
 // /api/wallet
 router.get("/my-balance", authMiddleware, getMyBalance);
 router.get("/summary", authMiddleware, getMySummary);
+router.post("/topup", authMiddleware, topup);
+router.post("/confirm-topup", authMiddleware, confirmTopup);
 
 export default router;
